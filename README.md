949fc5bedb2323d9c6e5add52670240d7fe300f5
OguzhanCevik <oguzhan.cevik@pera.software>
2022-11-27 00:58:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# roplanify :truck: :cyclone: :hourglass:
+
+``` 
+Route Optimizer Software
+```
+
+### License :key:
+Distributed under the MIT License. See [LICENSE](LICENSE) for more information.
---
+++ src/main/java/software/pera/roplanify/RoplanifyApplication.java
@@ -0,0 +1,13 @@
+package software.pera.roplanify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoplanifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoplanifyApplication.class, args);
+	}
+
+}
